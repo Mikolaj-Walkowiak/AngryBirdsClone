e41c0845442e557e92765d353398d5e89d598c43
Mikolaj-Walkowiak <mwalkowiak97@gmail.com>
2019-04-07 14:00:18 +0200

    1st draft

---
+++ Assets/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private bool isPressed = false;
+    public float releaseTime = 0.12f;
+    public float maxDragDistance = 4.0f;
+    public Rigidbody2D Anchor;
+
+    public GameObject nextball;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(MousePos, Anchor.position) > maxDragDistance)
+            {
+                rb.position = Anchor.position + (MousePos - Anchor.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rb.position = MousePos;
+            }
+
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+
+    }
+
+    // Update is called once per frame
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(3f);
+        if(nextball != null) nextball.SetActive(true);
+        else
+        {
+            Debug.Log("Przegranko xD Lamus");
+            EnemyDed.EnemiesAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+}
---
+++ Assets/EnemyDed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EnemyDed : MonoBehaviour
+{
+    public static int EnemiesAlive = 0;
+    public GameObject deathEffect;
+    public float Damage = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        if (colInfo.relativeVelocity.magnitude > Damage)
+        {
+            Kill();
+        }
+    }
+
+    void Kill()
+    {
+        
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            Debug.Log("Wygranko");
+            ///Next level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
